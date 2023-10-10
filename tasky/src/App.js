d7f063c39ff17de930b985bba8e08bf0ef532533
dlaw4608 <dlawton032@gmail.com>
2023-10-10 12:30:45 +0100

    form check

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -2,30 +2,48 @@ import logo from './logo.svg';
 import './App.css';
 import Task from './components/Task'
 import React, {useState} from 'react';
-
+import AddTaskForm from './components/Form';
 
 function App() {
 
   const [ taskState, setTaskState ] = useState({
     tasks: [
-      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
-      { id: 3, title: "Tidy up", deadline: "Today" },
-      { id: 4, title: "Clean Windowns", description:"Clean all of the upstair windows", deadline: "Thursday"},
-      { id: 5, title: "Wash Dog", description:"Wash and Dry the Dog", deadline: "Tonight"}
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", done: false },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false },
+      { id: 3, title: "Tidy up", deadline: "Today", done: false}
     ]
   });
+  const doneHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    setTaskState({tasks});
+    console.log(`${taskIndex} ${tasks[taskIndex].done}`);
+  }
+  
+
+
+  const deleteHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks.splice(taskIndex, 1);
+    setTaskState({tasks});
+  } 
   return (
     <div className="container">
       <h1>Tasky</h1>
-      {taskState.tasks.map((task) => (              
-   <Task 
-   title={task.title}
-   description={task.description}
-   deadline={task.deadline}
-   key={task.id}
- />
-  ))} 
+      {taskState.tasks.map((task, index) => (              
+    <Task 
+      title={task.title}
+      description={task.description}
+      deadline={task.deadline}
+      key={task.id}
+      done={task.done}
+      markDone={() => doneHandler(index)}
+      deleteTask = {() => deleteHandler(index)}
+      
+      
+    />
+  ))}
+  
     </div>
   );
  
